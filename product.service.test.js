65a891b05b7d593fbc1057653f49159c3acb0565
Julian <juliangolo@yahoo.es>
2026-07-28 20:12:19 +0200

    test: verificar excepcion con expect.toThrow en descuentos invalidos

--- product.service.test.js
+++ product.service.test.js
@@ -2,17 +2,29 @@
 import ProductService from "./product.service.js";
 
 describe("ProductService - Pruebas Unitarias", () => {
+  // 1. TEST 1: Camino Feliz (Happy Path)
   it("debería calcular correctamente el precio final con el 21% de IVA", () => {
     // 1. ARRANGE: (Preparar el entorno y las dependencias)
     const productService = new ProductService();
 
     // 2. ACT: (Ejecutar la función que queremos probar)
-    const precioBase = 120.5;
-    const resultado = productService.calcularPrecioConIVA(precioBase);
+    const resultado = productService.calcularPrecioConIVA(120.5);
 
     // 3. ASSERT: (Verificar que el resultado es el esperado)
     expect(resultado).toBe(145.805);
   });
-});
 
-export default ProductService;
+  // 2. TEST 2: Camino del error (Sad Path)
+  it("debería lanzar un error si el porcentaje de descuento es mayor o igual a 100", () => {
+    // 1. ARRANGE
+    const productService = new ProductService();
+
+    // 2 Act & Assert combinados mediante el callback en expect()
+    // Envolvemos la llamada al método applyCategoryDiscount dentro del arrow function
+    expect(() =>
+      productService
+        .applyCategoryDiscount("herramientas", 150)
+        .toThrow("El porcentaje debe estar entre 1 y 99"),
+    );
+  });
+});
